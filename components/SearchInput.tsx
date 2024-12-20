a2c231536977b31ade00eabe2776472169c0b056
Nahom Bekele <nahom.fix@gmail.com>
2024-12-20 12:39:54 +0300

    feat: add movie searching

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,12 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { FC } from "react";
 
-export const SearchInput: FC = () => {
+interface SearchInputProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export const SearchInput: FC<SearchInputProps> = ({ value, onChange }) => {
     return (
         <div className="flex flex-col gap-4 relative">
             <MagnifyingGlassIcon className="h-6 w-6 absolute top-1/2 left-4 -translate-y-1/2" />
@@ -9,6 +14,8 @@ export const SearchInput: FC = () => {
                 type="text"
                 placeholder="Search"
                 className="bg-[#1D1D1D] rounded-[14px] p-4 pl-12 outline-none"
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
             />
         </div>
     );
