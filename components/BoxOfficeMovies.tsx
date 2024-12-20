ec1b4dd8e0dc3d26bd8643e27a2ead8a2dc9aee4
Nahom Bekele <nahom.fix@gmail.com>
2024-12-20 12:23:09 +0300

    feat: add fetching box office movies

--- components/BoxOfficeMovies.tsx
+++ components/BoxOfficeMovies.tsx
@@ -1,38 +1,36 @@
 "use client";
 
+import { getBoxOfficeMovies } from "@/services/api";
 import { Movie } from "@/types/movie";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Thumbnails } from "./Thumbnails";
 
 export const BoxOfficeMovies: FC = () => {
-    const [boxOfficeMovies, setBoxOfficeMovies] = useState<Movie[]>([
-        {
-            Title: "The Gentlemen",
-            video_url: "https://gama-test-1.onrender.com/public/gentlmen.mp4",
-            cover_img_url:
-                "https://gama-test-1.onrender.com/public/gentlmen.jpg",
-            rating: 8.5,
-        },
-        {
-            Title: "The Gentlemen",
-            video_url: "https://gama-test-1.onrender.com/public/gentlmen.mp4",
-            cover_img_url:
-                "https://gama-test-1.onrender.com/public/gentlmen.jpg",
-            rating: 8.5,
-        },
-        {
-            Title: "The Gentlemen",
-            video_url: "https://gama-test-1.onrender.com/public/gentlmen.mp4",
-            cover_img_url:
-                "https://gama-test-1.onrender.com/public/gentlmen.jpg",
-            rating: 8.5,
-        },
-    ]);
+    const [boxOfficeMovies, setBoxOfficeMovies] = useState<Movie[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isError, setIsError] = useState<boolean>(false);
+
+    const fetchBoxOfficeMovies = async () => {
+        try {
+            const movies = await getBoxOfficeMovies();
+            setBoxOfficeMovies(movies);
+        } catch (error) {
+            console.error(error);
+            setIsError(true);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchBoxOfficeMovies();
+    }, []);
 
     return (
         <div className="flex flex-col gap-2">
             <h2 className="text-xl">Box Office Movies</h2>
-            <Thumbnails movies={boxOfficeMovies} />
+            {!isLoading && !isError && <Thumbnails movies={boxOfficeMovies} />}
+            {isError && <p>Failed to load box office movies</p>}
         </div>
     );
 };
