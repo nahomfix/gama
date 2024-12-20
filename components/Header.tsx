ab4ce05bb51bc569f5785ec5ee4943e48a459eaa
Nahom Bekele <nahom.fix@gmail.com>
2024-12-20 11:45:22 +0300

    feat: add search opening and closing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,32 @@
+"use client";
+
+import { searchModalAtom } from "@/store/searchModalAtom";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { useSetAtom } from "jotai";
 import Link from "next/link";
 import { FC } from "react";
 import { SearchOverlay } from "./SearchOverlay";
 import { SearchPopup } from "./SearchPopup";
 
 export const Header: FC = () => {
+    const toggleSearchModal = useSetAtom(searchModalAtom);
+
     return (
         <div className="flex items-center justify-between fixed md:static z-10 w-full py-4 px-8 bg-gradient-to-b from-black to-transparent">
             <Link href="/">
                 <p className="text-lg font-bold">GAMMA</p>
             </Link>
-            <MagnifyingGlassIcon className="h-6 w-6 md:hidden" />
+            <MagnifyingGlassIcon
+                className="h-6 w-6 md:hidden"
+                onClick={() => toggleSearchModal(true)}
+            />
             <div className="relative hidden md:block">
                 <MagnifyingGlassIcon className="h-6 w-6 absolute top-1/2 left-4 -translate-y-1/2" />
                 <input
                     type="text"
                     placeholder="What do you want to watch?"
                     className="bg-[#1D1D1D] rounded-[14px] p-3 pl-12 outline-none w-[350px]"
+                    onFocus={() => toggleSearchModal(true)}
                 />
                 <SearchPopup />
             </div>
