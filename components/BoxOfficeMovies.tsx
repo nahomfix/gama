2f2648fab1cfba69adc17fce8b95fb94b978240b
Nahom Bekele <nahom.fix@gmail.com>
2024-12-20 10:26:03 +0300

    feat: add box office section

--- components/BoxOfficeMovies.tsx
+++ components/BoxOfficeMovies.tsx
@@ -4,8 +4,8 @@ import { Movie } from "@/types/movie";
 import { FC, useState } from "react";
 import { Thumbnails } from "./Thumbnails";
 
-export const RecentMovies: FC = () => {
-    const [recentMovies, setRecentMovies] = useState<Movie[]>([
+export const BoxOfficeMovies: FC = () => {
+    const [boxOfficeMovies, setBoxOfficeMovies] = useState<Movie[]>([
         {
             Title: "The Gentlemen",
             video_url: "https://gama-test-1.onrender.com/public/gentlmen.mp4",
@@ -31,8 +31,8 @@ export const RecentMovies: FC = () => {
 
     return (
         <div className="flex flex-col gap-2">
-            <h2 className="text-xl">Recent Movies</h2>
-            <Thumbnails movies={recentMovies} />
+            <h2 className="text-xl">Box Office Movies</h2>
+            <Thumbnails movies={boxOfficeMovies} />
         </div>
     );
 };
