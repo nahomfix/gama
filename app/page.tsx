0b19aee7a4bade81052410e3806270f5496bcfc6
Nahom Bekele <nahom.fix@gmail.com>
2024-12-20 09:59:33 +0300

    feat: add recent movies section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { RecentMovies } from "@/components/RecentMovies";
 
 export default function Home() {
     return (
         <div className="relative">
             <Header />
             <Hero />
+            <div className="pl-4">
+                <RecentMovies />
+            </div>
         </div>
     );
 }
---
+++ components/RecentMovies.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const RecentMovies: FC = () => {
+    return (
+        <div className="flex flex-col gap-4">
+            <h2 className="text-xl">Recent Movies</h2>
+        </div>
+    );
+};
